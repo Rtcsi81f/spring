70ce3adc2d4abead531d30b4d47ab8d3a520c32d
杨海波 <3546514206@qq.com>
2021-09-16 16:19:22 +0800

    初始提交

---
+++ spring-loader/src/main/java/edu/zjnu/Person.java
@@ -0,0 +1,18 @@
+package edu.zjnu;
+
+/**
+ * @description: Person
+ * @author: 杨海波
+ * @date: 2021-08-11
+ **/
+public class Person {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ spring-loader/src/main/java/edu/zjnu/XmlBeanFactoryMain.java
@@ -0,0 +1,16 @@
+package edu.zjnu;
+
+/**
+ * @description: XmlBeanFactory的方式使用Spring容器
+ * @author: 杨海波
+ * @date: 2021-08-11
+ **/
+@SuppressWarnings("deprecation")
+public class XmlBeanFactoryMain {
+
+    public static void main(String[] args) {
+//        BeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("spring-ioc-common.xml"));
+//        Person person = (Person) beanFactory.getBean("person");
+//        System.out.println(person.getName());
+    }
+}
